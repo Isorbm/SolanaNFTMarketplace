e8f25c0d29c6d3d9e7022dc9b935e9ad1aaf54a1
Isorbm <Isorbm@users.noreply.github.com>
2024-06-05 20:53:59 +0300

    improve code readability

--- models/nft.js
+++ models/nft.js
@@ -1,28 +1,81 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
-const { Schema } = mongoose;
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
-const userSchema = new Schema({
-  username: { type: String, required: true, unique: true },
-  email: { type: String, required: true, unique: true },
-  walletAddress: { type: String, required: true, unique: true }
+const mongoDB = process.env.MONGODB_URI;
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+const userSchema = new mongoose.Schema({
+  username: { 
+    type: String, 
+    required: true, 
+    unique: true 
+  },
+  email: { 
+    type: String, 
+    required: true, 
+    unique: true 
+  },
+  walletAddress: { 
+    type: String, 
+    required: true, 
+    unique: true 
+  }
 });
-const nftSchema = new Schema({
-  tokenId: { type: String, required: true, unique: true },
-  owner: { type: Schema.Types.ObjectId, ref: 'User' },
-  metadataUri: { type: String, required: true },
-  name: { type: String, required: true },
-  description: { type: String },
-  imageUri: { type: String, required: true },
-  price: { type: Number, required: true },
-  isListed: { type: Boolean, required: true }
+const nftSchema = new mongoose.Schema({
+  tokenId: { 
+    type: String, 
+    required: true, 
+    unique: true 
+  },
+  owner: { 
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: 'User'
+  },
+  metadataUri: { 
+    type: String, 
+    required: true 
+  },
+  name: { 
+    type: String, 
+    required: true 
+  },
+  description: { 
+    type: String 
+  },
+  imageUri: { 
+    type: String, 
+    required: true 
+  },
+  price: { 
+    type: Number, 
+    required: true 
+  },
+  isListed: { 
+    type: Boolean, 
+    required: true 
+  }
 });
-const transactionSchema = new Schema({
-  nft: { type: Schema.Types.ObjectId, ref: 'NFT' },
-  seller: { type: Schema.Types.ObjectId, ref: 'User' },
-  buyer: { type: Schema.Types.ObjectId, ref: 'User' },
-  price: { type: Number, required: true },
-  transactionDate: { type: Date, default: Date.now }
+const transactionSchema = new mongoose.Schema({
+  nft: { 
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: 'NFT' 
+  },
+  seller: { 
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: 'User' 
+  },
+  buyer: { 
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: 'User' 
+  },
+  price: { 
+    type: Number, 
+    required: true 
+  },
+  transactionDate: { 
+    type: Date, 
+    default: Date.now 
+  }
 });
 const User = mongoose.model('User', userSchema);
 const NFT = mongoose.model('NFT', nftSchema);
