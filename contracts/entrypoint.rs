400f5c413c806e39135a1f8d5114ba6120614667
Isorbm <Isorbm@users.noreply.github.com>
2024-06-22 13:24:42 +0300

    update error catching

--- contracts/entrypoint.rs
+++ contracts/entrypoint.rs
@@ -3,21 +3,36 @@ use solana_program::{
     entrypoint,
     entrypoint::ProgramResult,
     msg,
+    program_error::ProgramError,
     pubkey::Pubkey,
 };
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum MarketplaceError {
+    InvalidInstruction,
+}
+
+impl From<MarketplaceError> for ProgramError {
+    fn from(e: MarketplaceError) -> Self {
+        ProgramError::Custom(e as u32)
+    }
+}
 
 entrypoint!(process_instruction);
 
 fn process_instruction(
-    program_id: &Pubkey, 
-    accounts: &[AccountInfo], 
-    instruction_data: &[u8], 
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    instruction_data: &[u8],
 ) -> ProgramResult {
     msg!("Hello, Solana program!");
 
     let accounts_iter = &mut accounts.iter();
 
-    let account = next_account_info(accounts_iterator)?;
+    let account = match next_account_info(accounts_iter) {
+        Ok(account_info) => account_info,
+        Err(_) => return Err(MarketplaceError::InvalidInstruction.into()),
+    };
 
     Ok(())
 }
