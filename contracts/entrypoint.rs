edc23637bb4db81b415e282807a6796577b63a7d
Isorbm <Isorbm@users.noreply.github.com>
2024-06-25 11:02:32 +0300

    optimization

--- contracts/entrypoint.rs
+++ contracts/entrypoint.rs
@@ -6,6 +6,8 @@ use solana_program::{
     program_error::ProgramError,
     pubkey::Pubkey,
 };
+use std::collections::HashMap;
+use std::cell::RefCell;
 
 #[derive(Debug, Clone, PartialEq)]
 pub enum MarketplaceError {
@@ -18,10 +20,14 @@ impl From<MarketplaceError> for ProgramError {
     }
 }
 
+thread_local! {
+    static INSTRUCTION_DATA_CACHE: RefCell<HashMap<Vec<u8>, String>> = RefCell::new(HashMap::new());
+}
+
 entrypoint!(process_instruction);
 
 fn process_instruction(
-    program_id: &Pubkey,
+    program_id: &PubwestPubkey,
     accounts: &[AccountInfo],
     instruction_data: &[u8],
 ) -> ProgramResult {
@@ -34,5 +40,21 @@ fn process_instruction(
         Err(_) => return Err(MarketplaceError::InvalidInstruction.into()),
     };
 
+    let parsed_instruction = parse_instruction_data_cached(instruction_data);
+
+    msg!("Parsed Instruction: {}", parsed_parse_instruction);
+
     Ok(())
+}
+
+fn parse_instruction_data_cached(instruction_data: &[u8]) -> String {
+    INSTRUCTION_DATA_CACHE.with(|cache| {
+        let mut cache = cache.borrow_mut();
+        if let Some(result) = cache.get(instruction_data) {
+            return result.clone();
+        }
+        let result = String::from_utf8(instruction_data.to_vec()).unwrap_or_else(|_| "Invalid UTF-8".to_string());
+        cache.insert(instruction_data.to_vec(), result.clone());
+        result
+    })
 }
