1b9c0c5fd8d9016cf770a8a9b3abacdd275ebf44
Isorbm <Isorbm@users.noreply.github.com>
2024-07-07 11:10:06 +0300

    advance the readability

--- contracts/state.rs
+++ contracts/state.rs
@@ -1,5 +1,5 @@
 use anchor_lang::prelude::*;
-use anchor_lang::solana_program::entrypoint::ProgramResult;
+use anchor_lang::solana_program::entrypoint::ProgramError;
 
 #[account]
 pub struct Nft {
@@ -18,16 +18,19 @@ pub struct Marketplace {
 }
 
 impl Marketplace {
-    pub fn add_nft(&mut self, nft_pubkey: Pubkey) -> ProgramResult {
+    pub fn add_nft(&mut self, nft_pubkey: Pubkey) -> Result<(), ProgramError> {
         require!(self.nfts.len() < 4294967295, ProgramError::AccountDataTooSmall);
 
         self.nfts.push(nft_pubkey);
         Ok(())
     }
 
-    pub fn remove_nft(&mut self, nft_pubkey: Pubkey) -> ProgramResult {
-        let index = self.nfts.iter().position(|&r| r == nft_pubkey).unwrap();
-        self.nfts.remove(index);
-        Ok(())
+    pub fn remove_nft(&mut self, nft_pubkey: Pubkey) -> Result<(), ProgramError> {
+        if let Some(index) = self.nfts.iter().position(|&r| r == nft_pubkey) {
+            self.nfts.remove(index);
+            Ok(())
+        } else {
+            Err(ProgramError::InvalidArgument)
+        }
     }
 }
